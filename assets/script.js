d144a757faaa4b6c00b91a498708c03f7a3abc55
GameW1zard <rivenloya@gmail.com>
2023-03-06 15:42:14 -0600

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -2,16 +2,32 @@
 var awnsers = document.querySelector("#awnsers")
 var question = document.querySelector("#questions")
 var countdown = document.querySelector("#counter")
-var counter = 60
+var counter = 90
 var score = 100
 var awnser1 = false
 var awnser2 = false
 var awnser3 = false
 var awnser4 = false
+var initals = ""
+
+
 countdown.textContent = counter
 
-timer()
 
+
+function startquiz() {
+  var li1 = document.createElement("li")
+
+  question.textContent = "click start to begin quiz"
+
+  li1.textContent = "Click to start!"
+  li1.setAttribute("id", "li1")
+  awnsers.appendChild(li1)
+  li1.addEventListener ("click", function() {question1(), timer(), li1.remove()})
+
+}
+
+initsubmit
 
 function question1() {
     var li1 = document.createElement("li")
@@ -27,8 +43,8 @@ function question1() {
     awnsers.appendChild(li1)
     awnsers.appendChild(li2)
 
-    li1.addEventListener ("click", function() {console.log("li1 clicked " + score), awnser1 = true })
-    li2.addEventListener ("click", function() {score -= 10, counter -= 5, console.log("li2 clicked " + score) })
+    li1.addEventListener ("click", function() {console.log("li1 clicked " + score), awnser1 = true, li1.remove(), li2.remove(), question2()})
+    li2.addEventListener ("click", function() {score -= 10, counter -= 5, console.log("li2 clicked " + score)})
 
 
 
@@ -53,19 +69,98 @@ function question2() {
   awnsers.appendChild(li2)
   awnsers.appendChild(li3)
   
-  li1.addEventListener ("click", function() {console.log("li1 clicked " + score), awnser1 = true })
+  li1.addEventListener ("click", function() {console.log("li1 clicked " + score), li1.remove(), li2.remove(), li3.remove(), awnser2 = true, question3()})
   li2.addEventListener ("click", function() {score -= 10, counter -= 5, console.log("li2 clicked " + score) })
-  li3.addEventListener ("click", function)
+  li3.addEventListener ("click", function() {score -= 10, counter -= 5, console.log("li3 clicked " + score) })
+
+
+
+}
+
+function question3() {
+  var li1 = document.createElement("li")
+  var li2 = document.createElement("li")
+  var li3 = document.createElement("li")
+  var li4 = document.createElement("li")
+  question.textContent = "How many body pillows do i own"
+
+  li1.textContent = "1"
+  li2.textContent = "+5"
+  li3.textContent = "3"
+  li4.textContent = "All of them"
+
+  li1.setAttribute("id", "li1")
+  li2.setAttribute("id", "li2")
+  li3.setAttribute("id", "li3")
+  li4.setAttribute("id", "li4")
+  awnsers.appendChild(li1)
+  awnsers.appendChild(li2)
+  awnsers.appendChild(li3)
+  awnsers.appendChild(li4)
+  li1.addEventListener ("click", function() {console.log("li1 clicked " + score), awnser3 = true, li1.remove(), li2.remove(), li3.remove(), li4.remove(), question4()})
+  li2.addEventListener ("click", function() {score -= 10, counter -= 5, console.log("li2 clicked " + score)})
+  li3.addEventListener ("click", function() {score -= 10, counter -= 5, console.log("li3 clicked " + score)})
+  li4.addEventListener ("click", function() {score -= 10, counter -= 5, console.log("li4 clicked " + score)})
+
+
+
+}
+
+function question4() {
+  var li1 = document.createElement("li")
+  var li2 = document.createElement("li")
+  question.textContent = "Where is mika"
+
+  li1.textContent = "Balls deep in ice"
+  li2.textContent = "Asleep"
+
+  li1.setAttribute("id", "li1")
+  li2.setAttribute("id", "li2")
+
+  awnsers.appendChild(li1)
+  awnsers.appendChild(li2)
+
+  li1.addEventListener ("click", function() {console.log("li1 clicked " + score), awnser4 = true, li1.remove(), li2.remove()})
+  li2.addEventListener ("click", function() {score -= 10, counter -= 5, console.log("li2 clicked " + score)})
+
+
+
+
+}
+
+//function Form(event) {
+  var inputholder = document.createElement("form")
+  var nameinput = document.createElement("input")
+  var uldel = document.querySelector("#awnsers")
+  var flexholder = document.querySelector("#flexholder")
+  var button = document.createElement("submit")
+  
+  question.textContent = "Please enter your initals";
+
+  uldel.remove();
+  inputholder.setAttribute("method", "post");
+  nameinput.setAttribute("type", "text");
+
+
+console.log(initals)
+
+
+
+//}
+
+function initsubmit() {
+
+  initals = prompt("Your score was " + score + "please enter your initals", "W1z")
+
+
 
 
 
 }
 
 
-
-
 function timer() {
-    counter = 60;
+    counter = 90;
   
     var timeInterval = setInterval(function () {
     countdown.textContent = counter + " seconds left"
@@ -82,7 +177,6 @@ function timer() {
     }, 1000);
   }
 
-question1()
 
 
 
